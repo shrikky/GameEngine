f36f9bd62a3c584e83f41f657983dc334759afe3
Srinivasan <tsrivishnu99@github.com>
2015-11-30 10:31:11 -0500

    shader changed

--- Game Engine/shaders/simple_frag.glsl
+++ Game Engine/shaders/simple_frag.glsl
@@ -1,12 +1,53 @@
 #version 440 core
 
+#define MAX_LIGHTS 10
+
 in vec2 TexCoords;
+in vec3 normal;
+in vec3 posOut;
 
 out vec4 color;
 
+struct Light
+{
+	vec4 position;
+	vec3 intensity;
+};
+
 uniform sampler2D texture_diffuse1;
+uniform int numLights;
+uniform Light lights[MAX_LIGHTS];
+
+vec4 diffuse()
+{
+	vec4 lightEffect = vec4(0.0f,0.0f,0.0f,0.0f);
+	vec3 tempPosition;
+	vec3 lightPath;
+	float intensity;
+
+	for (int i = 0; i < numLights; i++)
+	{
+		if (lights[i].position.w == 1)					// Point light
+		{
+			tempPosition = lights[i].position.xyz;
+			lightPath = tempPosition - posOut;
+		}
+		else											// Directional Light
+		{
+			lightPath = lights[i].position.xyz;
+		}
+
+		intensity = max(dot(lightPath, normal), 0.0f);
+		lightEffect += vec4(lights[i].intensity, 0.0f) * intensity;
+	}
+
+	lightEffect.w = 1.0f;
+
+	return lightEffect;
+}
 
 void main()
-{    
-    color = vec4(texture(texture_diffuse1, TexCoords));
+{   
+
+	color = vec4(texture(texture_diffuse1, TexCoords)) * diffuse();
 }
--- Game Engine/shaders/simple_vert.glsl
+++ Game Engine/shaders/simple_vert.glsl
@@ -4,6 +4,8 @@ layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 texCoords;
 
 out vec2 TexCoords;
+out vec3 normal;
+out vec3 posOut;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,5 +14,7 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(position, 1.0f);
+	normal = normalize(position);
+	posOut = position;
     TexCoords = texCoords;
 }
